b6139d14030ae9b78a801a80bab40fda098abd3e
Anshul <12948312+k-anshul@users.noreply.github.com>
2022-10-17 22:16:36 +0530

    Update AuctionState.java

--- src/main/java/com/RillAuction/constants/AuctionState.java
+++ src/main/java/com/RillAuction/constants/AuctionState.java
@@ -4,12 +4,8 @@ import lombok.Getter;
 
 @Getter
 public enum AuctionState {
-    CREATED(10),
-    ACTIVE(20),
-    COMPLETED(30),
-    ARCHIVED(40);
-
-
-    private int id;
-    AuctionState(int id) {this.id = id;}
+    CREATED,
+    ACTIVE,
+    COMPLETED,
+    ARCHIVED;
 }
